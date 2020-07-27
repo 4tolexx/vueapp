75efcb489fe5103e5e399138aa7334ab69aaaa72
Tolex <stevenreal739@gmail.com>
2020-07-27 05:11:25 +0100

    created TochukwuUzochukwu.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TochukwuUzochukwu MyName="Tochukwu Uzochukwu"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TochukwuUzochukwu from './components/TochukwuUzochukwu.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TochukwuUzochukwu
   }
 }
 </script>
